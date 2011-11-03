582f20e4564b1f9fb96d4936c3a227a08ac1f7db
Chris Mungall <cjm@berkeleybop.org>
2011-11-02 17:39:59 -0700

    new

---
+++ process-RefGenomeOrthologs.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+while(<>) {
+    chomp;
+    my @vals = split(/\t/);
+    if (@vals == 6) {
+        splice(@vals,3,2,("$vals[3]/$vals[4]"));
+    }
+    my ($x,$y,$rel,$anc,$fam) = @vals;
+    print join("\t",
+               (proc_idstr($x),
+                proc_idstr($y),
+                $rel,
+                $anc,
+                $fam))."\n";
+}
+exit 0;
+
+sub proc_idstr {
+    my ($sp,$mod,$pr) = split(/\|/,$_[0]);
+    return (proc_id($mod),proc_id($pr));
+}
+
+sub proc_id {
+    return join(':',split(/=/,$_[0]));
+}
